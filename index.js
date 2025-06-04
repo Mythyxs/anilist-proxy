98bcfb969ad17647a99fee2598a9c1ae72d121dc
Mythyxs <shemxz123@gmail.com>
2025-06-04 14:28:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -96,3 +96,4 @@ app.options('/anilist', (req, res) => res.sendStatus(200));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🌐 Server running on port ${PORT}`));
+generateCachedSchedule();
